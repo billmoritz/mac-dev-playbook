300056e2c2d326a1b7296627c925ac15b70d5322
Bill Moritz <wmoritz@salesforce.com>
2019-10-16 23:29:11 -0400

    Update install script

--- install.sh
+++ install.sh
@@ -1,18 +1,5 @@
 #!/usr/bin/env bash
 
-# Simple Install Script to Set Up a New Mac with One Command Using Curl
-# What it does:
-# - Install pip (if needed) and then install Ansible with pip
-# - Install the xcode command line tools
-# - Clone Ansible Playbook with git
-# - Run Ansible Playbook to Configure New Mac
-
-# Ask for the administrator password upfront
-sudo -v
-
-# Keep-alive: update existing `sudo` time stamp until `install.sh` has finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
 if [ ! -x "$(command -v pip)" ]; then
     sudo easy_install pip
 fi
@@ -25,12 +12,12 @@ if [ ! -x "$(command -v cc)" ]; then
     sudo xcode-select --install
 fi
 
-git clone https://github.com/billmoritz/mac-dev-playbook.git ~/mac-dev-playbook || exit
+if [ ! -d "$HOME/mac-dev-playbook/.git" ]; then
+  git clone https://github.com/billmoritz/mac-dev-playbook.git "$HOME/mac-dev-playbook" || exit
+fi
 
-cd ~/mac-dev-playbook || exit
+cd "$HOME/mac-dev-playbook" || exit
 
 ansible-galaxy install -r requirements.yml
 
 ansible-playbook main.yml -i inventory -K
-
-
